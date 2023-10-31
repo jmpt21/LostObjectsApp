c3a34af4fbb4b91f442f6e2573889588e1212867
JoseMPT <s20030132@itsch.edu.mx>
2023-10-31 01:15:16 -0600

    Login module. Patch. Cleaning of Main

--- app/src/main/java/mx/tecnm/cdhidalgo/lostobjectsapp/MainActivity.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/lostobjectsapp/MainActivity.kt
@@ -2,19 +2,14 @@ package mx.tecnm.cdhidalgo.lostobjectsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import mx.tecnm.cdhidalgo.lostobjectsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
