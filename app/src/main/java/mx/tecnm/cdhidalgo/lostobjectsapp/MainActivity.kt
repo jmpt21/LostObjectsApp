9c4a3faf51c7de965396633452f322ff233e679e
JoseMPT <s20030132@itsch.edu.mx>
2023-10-30 23:47:39 -0600

    Login module. Part II. Presentation design

--- app/src/main/java/mx/tecnm/cdhidalgo/lostobjectsapp/MainActivity.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/lostobjectsapp/MainActivity.kt
@@ -2,10 +2,25 @@ package mx.tecnm.cdhidalgo.lostobjectsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import mx.tecnm.cdhidalgo.lostobjectsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        val navController = findNavController(R.id.nav_host_fragment_content_login_module)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
