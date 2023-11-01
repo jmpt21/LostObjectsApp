a2c98faa89789211d374546daafd8d646f0f3f4b
JoseMPT <s20030132@itsch.edu.mx>
2023-11-01 00:47:02 -0600

    Login module. Part III. Login View completed

--- app/src/main/java/mx/tecnm/cdhidalgo/lostobjectsapp/fragments/PresentationFragment.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/lostobjectsapp/fragments/PresentationFragment.kt
@@ -17,11 +17,6 @@ class PresentationFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-/*    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }*/
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,9 +30,6 @@ class PresentationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }*/
         binding.btnSignInPresentation.setOnClickListener {
             findNavController().navigate(R.id.action_PresentationFragment_to_LoginFragment)
         }
@@ -47,23 +39,8 @@ class PresentationFragment : Fragment() {
         }
     }
 
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment PresentationFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            PresentationFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
