82186d10bbdb262a6126e450acb2416068d0a86a
JoseMPT <s20030132@itsch.edu.mx>
2023-11-01 16:19:44 -0600

    Login module. Part IV. Recovery pass view was completed.

--- app/src/main/java/mx/tecnm/cdhidalgo/lostobjectsapp/fragments/RecoveryPassFragment.kt
+++ app/src/main/java/mx/tecnm/cdhidalgo/lostobjectsapp/fragments/RecoveryPassFragment.kt
@@ -5,56 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import mx.tecnm.cdhidalgo.lostobjectsapp.R
+import mx.tecnm.cdhidalgo.lostobjectsapp.databinding.FragmentRecoveryPassBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [RecoveryPassFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecoveryPassFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private var _binding : FragmentRecoveryPassBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recovery_pass, container, false)
+        _binding = FragmentRecoveryPassBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecoveryPassFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RecoveryPassFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnBackRecoveryPass.setOnClickListener {
+            findNavController().navigate(R.id.action_RecoveryPassFragment_to_LoginFragment)
+        }
+        binding.btnButtonLogin.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
